929971855dc6b0bd3ef3b37b3ce1da7fce50d99f
yujinio <root@yujin.io>
2024-05-30 12:11:17 +0200

    rm gunicorn conf

--- backend/giftbook/gunicorn.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing as mp
-import os
-
-host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", 3000)
-bind = f"{host}:{port}"
-workers = os.getenv("WEB_CONCURRENCY", mp.cpu_count() * 2 + 1)
-worker_class = "uvicorn.workers.UvicornWorker"
