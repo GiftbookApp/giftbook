a780c6e0d4a8e6312241f792874ce9c2d47575b7
yujinio <root@yujin.io>
2024-05-30 14:53:13 +0200

    rework user table removing dependency on piccolo base user

--- backend/giftbook/tables/users.py
+++ backend/giftbook/tables/users.py
@@ -1,27 +1,22 @@
 from datetime import datetime, timezone
 
-from piccolo.apps.user.tables import BaseUser
 from piccolo.columns import UUID, Boolean, Email, Secret, Timestamptz, Varchar
 from piccolo.columns.readable import Readable
+from piccolo.table import Table
 
 from giftbook.db import DB
 
 
-class User(BaseUser, db=DB, tablename="users"):
+class User(Table, db=DB, tablename="users"):
     id = UUID(primary_key=True)
     username = Varchar(length=255, required=True, unique=True)
-    full_name = Varchar(length=255)
-    email = Email(index=True)
     password_hash = Secret(length=128)
+    email = Email(index=True)
     is_email_verified = Boolean(default=False)
+    full_name = Varchar(length=255)
     is_admin = Boolean(default=False)
     created_at = Timestamptz(default=datetime.now(timezone.utc))
     modified_at = Timestamptz(auto_update=datetime.now(timezone.utc))
-
-    # overwritten to none
-    first_name = None
-    last_name = None
-    password = None
 
     @classmethod
     def get_readable(cls) -> Readable:
